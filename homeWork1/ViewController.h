5c0c57be3b974f9e487e4dba509086eebd80e4fc
Sergejs Lastovskis <Sergejs.lastovskis@gmail.com>
2016-06-29 10:58:27 +0300

    Initial Commit

---
+++ homeWork1/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  homeWork1
+//
+//  Created by Sergejs Lastovskis on 29/06/16.
+//  Copyright © 2016 Sergejs Lastovskis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
